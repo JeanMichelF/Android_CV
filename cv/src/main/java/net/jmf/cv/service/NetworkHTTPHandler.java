15cc8ca81569d41fd1e9500d8e9b75cdf3199adc
Jean-Michel F <cv@jeanmichelfahys.net>
2014-04-18 23:59:01 +0200

    - Forgot to commit some important files in order to handle Http requests...

---
+++ cv/src/main/java/net/jmf/cv/service/NetworkHTTPHandler.java
@@ -0,0 +1,34 @@
+package net.jmf.cv.service;
+
+import org.apache.http.client.methods.HttpUriRequest;
+
+/**
+ * Abstract class to implement in order to make an http call
+ * Created by Jean-Mi on 18/04/2014.
+ */
+public abstract class NetworkHttpHandler {
+    /**
+     * Get or Post...
+     * @return  HttpUriRequest
+     */
+    public abstract HttpUriRequest getHttpRequestMethod();
+
+    /**
+     * When succeeded
+     * @param result    String
+     */
+    public abstract void onResponse(String result);
+
+    /**
+     * When cancelled
+     * @param result    String
+     */
+    public abstract void onCancel(String result);
+
+    /**
+     * Make the call
+     */
+    public void execute() {
+        new NetworkHttpMethod(this).execute();
+    }
+}
---
+++ cv/src/main/java/net/jmf/cv/service/NetworkHTTPMethod.java
@@ -0,0 +1,71 @@
+package net.jmf.cv.service;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * Generic Class handling HTTP Methods and returning a String
+ * Created by Jean-Mi on 18/04/2014.
+ */
+public class NetworkHttpMethod extends AsyncTask<String, Void, String> {
+    private NetworkHttpHandler networkHttpHandler;
+
+    public NetworkHttpMethod(NetworkHttpHandler networkHttpHandler) {
+        this.networkHttpHandler = networkHttpHandler;
+    }
+
+    /**
+     * Make the call
+     * @param arg0  Args
+     * @return  String
+     */
+    @Override
+    protected String doInBackground(String... arg0) {
+        String result = "";
+        try {
+            //Create the HTTP request
+            HttpParams httpParameters = new BasicHttpParams();
+
+            //Setup timeouts
+            HttpConnectionParams.setConnectionTimeout(httpParameters, 1000);
+            HttpConnectionParams.setSoTimeout(httpParameters, 1000);
+
+            HttpClient httpClient = new DefaultHttpClient(httpParameters);
+            HttpResponse response = httpClient.execute(networkHttpHandler.getHttpRequestMethod());
+            HttpEntity entity = response.getEntity();
+
+            result = EntityUtils.toString(entity);
+
+        } catch (Exception e) {
+            Log.e("NetworkHTTPGet", "Error: ", e);
+        }
+        return result;
+    }
+
+    /**
+     * When fully loaded
+     * @param result    String
+     */
+    @Override
+    protected void onPostExecute(String result) {
+        networkHttpHandler.onResponse(result);
+    }
+
+    /**
+     * When disabled
+     * @param result    String
+     */
+    @Override
+    protected void onCancelled(String result) {
+        networkHttpHandler.onCancel(result);
+    }
+}
